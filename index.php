827f6f7e26a7a3a73543e3e18028481da146a519
NiKomangLeniGustiawati <125976780+NiKomangLeniGustiawati@users.noreply.github.com>
2023-02-26 12:35:18 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+   $number =1;
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website.</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+          
+            <div class="logo"><a href=''>LENIIII</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href ="#Home">Home</a></li>
+                    <li><a href ="#profile">profile</a></li>
+                    <li><a href ="#Contact">Contact</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+
+    </section>
+     <!--untuk home-->
+        <section id="Home">
+            <img src="https://img.freepik.com/free-vector/programmer-working-flat-style_52683-15041.jpg?w=740&t=st=1677298736~exp=1677299336~hmac=f6c49e59246976ade59b02f13fef09dfe9c3912a4e1ce287a45ff56e78415fd9"/>
+            <div class="kolom">
+                <p class="deskripsi">BELAJAR BERSAMA GITS</p>
+                <h2>Tetap semangat dan optimis</h2>
+                <p>Success is going from failure to failure without losing your enthusiasm.</p>
+    
+            </section>
+            <!--untuk profile-->
+            <section id="profile">
+                <div class="tengah">
+                    <div class="kolom">
+                        <p class="deskripsi">My Profile</p>
+                        <h2>Profile</h2>
+                    </div>
+
+                    <div class="profile">
+                        <div class="kartu-profile">
+
+                            <img src="https://cdn-image.hipwee.com/wp-content/uploads/2019/04/hipwee-photo-1529529149675-1226a5a3d888-1-334x422.jpg"/>
+                            <p>LENII</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+          </div>
+             <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>LENII</h3>
+                    <p>hello guys </p>
+                </div>
+                    <div class="footer-section">
+                        <h3>About</h3>
+                        <p>hello guys,perkenalkan nama saya Ni Komang Leni Gustiawati atau sering dipanggil Leni,saya mahasiswa semester 6 di kampus Universitas Teknologi Mataram,jurusan sistem informasi fakultas Teknologi Informasi dan komunikasi</p>
+                    </div>
+                     <div class="footer-section">
+                        <h3>Contact</h3>
+                        <p>Jl.Dusun pemunut,Desa suranadi,kecamatan Narmada,kabupaten Lombok Barat</p>
+                        <p> kode pos:83371</p>
+                    </div>
+                      <div class="footer-section">
+                        <h3>Social</h3>
+                        <p><b>Instagram: </b>manglenii</p>
+                    </div>
+                </div>
+             </div>
+       </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>LENII</b> All  Rights Reserved.
+        </div>
+    </div>
+</body>
+</html>
